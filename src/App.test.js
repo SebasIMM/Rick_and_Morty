d4368f30c481bb2b7a5c4266770aeeb3c919cc3d
Sebastian Medel <sevastian.ignis@gmail.com>
2023-10-16 21:17:19 -0300

    First React App - Rick and Morty

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText("Rick and Morty");
+  expect(titleElement).toBeInTheDocument();
 });
